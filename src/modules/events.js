a9cc129bfe123f413121fdc1f6d698a95d78aa58
XNAK3 <yagami01018@gmail.com>
2026-07-05 04:42:19 -0400

    Refactor src files to modularity behavior, continue

---
+++ src/modules/events.js
@@ -0,0 +1,30 @@
+export function setupValidationEvents() {
+    const form = document.querySelector("form");
+    const search = document.getElementById("search");
+    const btn = document.querySelector(".submit-btn");
+    const errorMsg = document.querySelector(".error");
+
+    function showError() {
+        if (search.validity.valueMissing) {
+            errorMsg.textContent = "It can't be empty";
+        }
+
+        errorMsg.className = "error active";
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (!search.validity.valid) {
+            showError();
+        }
+    });
+
+    search.addEventListener("input", () => {
+        if (search.validity.valid) {
+            errorMsg.textContent = "";
+            errorMsg.className = "error";
+            return;
+        }
+        showError();
+    });
+}
